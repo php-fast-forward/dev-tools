cfe9391f54ce0d32bac4aaff5509282c58d96561
Felipe Sayão Lobato Abreu <github@mentordosnerds.com>
2026-04-17 15:24:56 -0300

    [command] Fix SyncCommand InputOption import (#52) (#53)
    * Fix SyncCommand InputOption import
    
    * Update wiki submodule pointer for PR #53
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Console/Command/SyncCommand.php
+++ src/Console/Command/SyncCommand.php
@@ -20,11 +20,11 @@ declare(strict_types=1);
 namespace FastForward\DevTools\Console\Command;
 
 use Composer\Command\BaseCommand;
-use ECSPrefix202601\Symfony\Component\Console\Input\InputOption;
 use FastForward\DevTools\Process\ProcessBuilderInterface;
 use FastForward\DevTools\Process\ProcessQueueInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Path;
 
