eba429af4d71b7ce30922d174672853bf0cf62ea
coisa <coisa@users.noreply.github.com>
2026-04-30 05:00:47 +0000

    deploy: ccfecafa606b9d27db253783c9e9002bcdd6dae1

--- js/searchIndex.js
+++ js/searchIndex.js
@@ -3993,7 +3993,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\FastForward\\DevTools\\GitHooks\\HookContentRenderer\u003A\u003AMANAGED_GRUMPHP_CONFIG_PLACEHOLDER",
             "name": "MANAGED_GRUMPHP_CONFIG_PLACEHOLDER",
-            "summary": "Placeholder\u0020replaced\u0020with\u0020the\u0020active\u0020packaged\u0020GrumPHP\u0020config\u0020path.",
+            "summary": "Placeholder\u0020replaced\u0020with\u0020the\u0020packaged\u0020GrumPHP\u0020config\u0020path\u0020rendered\u0020relative\u0020to\u0020the\u0020project\u0020when\u0020possible.",
             "url": "classes/FastForward-DevTools-GitHooks-HookContentRenderer.html#constant_MANAGED_GRUMPHP_CONFIG_PLACEHOLDER"
         },                {
             "fqsen": "\\FastForward\\DevTools\\GitIgnore\\Classifier",
@@ -4321,6 +4321,11 @@ Search.appendIndex(
             "summary": "Returns\u0020the\u0020packaged\u0020resources\u0020directory\u0020or\u0020a\u0020path\u0020under\u0020it.",
             "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_getResourcesPath"
         },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AgetPackagePathRelativeToProject\u0028\u0029",
+            "name": "getPackagePathRelativeToProject",
+            "summary": "Returns\u0020a\u0020packaged\u0020path\u0020rendered\u0020relative\u0020to\u0020the\u0020active\u0020project\u0020root\u0020when\u0020possible.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_getPackagePathRelativeToProject"
+        },                {
             "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AgetRuntimeAutoloadPath\u0028\u0029",
             "name": "getRuntimeAutoloadPath",
             "summary": "Returns\u0020the\u0020active\u0020Composer\u0020autoload\u0020file\u0020for\u0020the\u0020current\u0020DevTools\u0020installation\u0020mode.",
@@ -4360,6 +4365,46 @@ Search.appendIndex(
             "name": "normalizeVendorRelativePath",
             "summary": "Normalizes\u0020a\u0020path\u0020relative\u0020to\u0020the\u0020Composer\u0020vendor\u0020root.",
             "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_normalizeVendorRelativePath"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AassertRelativePackagePath\u0028\u0029",
+            "name": "assertRelativePackagePath",
+            "summary": "Ensures\u0020packaged\u0020paths\u0020stay\u0020relative\u0020to\u0020the\u0020DevTools\u0020package\u0020root.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_assertRelativePackagePath"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AresolvePackageRelativePath\u0028\u0029",
+            "name": "resolvePackageRelativePath",
+            "summary": "Returns\u0020a\u0020canonical\u0020path\u0020under\u0020the\u0020DevTools\u0020package\u0020root.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_resolvePackageRelativePath"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AresolvePackageRoot\u0028\u0029",
+            "name": "resolvePackageRoot",
+            "summary": "Returns\u0020the\u0020canonical\u0020DevTools\u0020package\u0020root.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_resolvePackageRoot"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AresolveProjectPath\u0028\u0029",
+            "name": "resolveProjectPath",
+            "summary": "Returns\u0020the\u0020canonical\u0020working\u0020project\u0020root.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_resolveProjectPath"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AgetRuntimeVendorRoot\u0028\u0029",
+            "name": "getRuntimeVendorRoot",
+            "summary": "Returns\u0020the\u0020active\u0020Composer\u0020vendor\u0020root\u0020for\u0020the\u0020current\u0020DevTools\u0020installation\u0020mode.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_getRuntimeVendorRoot"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003AgetProjectVendorPath\u0028\u0029",
+            "name": "getProjectVendorPath",
+            "summary": "Returns\u0020a\u0020vendor\u0020path\u0020under\u0020the\u0020active\u0020project\u0020root.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_getProjectVendorPath"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003ApreferExistingPath\u0028\u0029",
+            "name": "preferExistingPath",
+            "summary": "Returns\u0020the\u0020preferred\u0020path\u0020when\u0020a\u0020project\u002Dlocal\u0020candidate\u0020exists.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_preferExistingPath"
+        },                {
+            "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003ArelativizePathFromProject\u0028\u0029",
+            "name": "relativizePathFromProject",
+            "summary": "Returns\u0020a\u0020path\u0020relative\u0020to\u0020the\u0020project\u0020root\u0020when\u0020possible.",
+            "url": "classes/FastForward-DevTools-Path-DevToolsPathResolver.html#method_relativizePathFromProject"
         },                {
             "fqsen": "\\FastForward\\DevTools\\Path\\DevToolsPathResolver\u003A\u003ABINARY",
             "name": "BINARY",
